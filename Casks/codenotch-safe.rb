03cd233f288cb42be10d5b4c1b23a85cdf3fc66a
daniil.tagan <daniil.tagan@piano.io>
2026-09-17 20:47:25 +0400

    style: group Cask stanzas

--- Casks/codenotch-safe.rb
+++ Casks/codenotch-safe.rb
@@ -13,8 +13,8 @@ cask "codenotch-safe" do
   homepage "https://github.com/x950827/codenotch-safe"
 
   depends_on macos: :sequoia
-
   container type: :dmg
+
   app "Codenotch Safe.app"
 
   caveats <<~EOS
